c4ca5e98c4886ff10185ae72391df3d8b9b44b21
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-08-14 20:11:30 +0100

    any sin

--- src/view/front/survey.php
+++ src/view/front/survey.php
@@ -11,72 +11,44 @@
                             <h1>
                                 Formulaire d'enquete
                             </h1>
+
                         <form class="row g-3 pt-2">
-                            <div class="row g-3">
-                                <div class="col-md-6">
-                                    <label for="name" class="text-white">Nom</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control" id="name" placeholder="Nom">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="email">Prénom</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control" id="email" placeholder="Prénom">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row g-3">
-                                <div class="col-md-6">
-                                    <label for="name" class="text-white">Votre date de naissance</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <input type="date" class="form-control" id="name" placeholder="Votre date de naissance">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="email">Votre sexe</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <select name="" id="" class="form-control">
-                                            <option value="">Veuillez sélectionner</option>
-                                            <option value="M">Masculin</option>
-                                            <option value="F">Féminin</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row g-3">
-                                <div class="col-md-6">
-                                    <label for="name" class="text-white">Numéro de téléphone</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <input type="number" class="form-control" id="name" placeholder="Votre nom">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="email">Arrondissement</label> <span class="red">*</span>
-                                    <div class="form-floating">
-                                        <input type="number" class="form-control" id="email" placeholder="Votre prénom">
-                                    </div>
-                                </div>
-                            </div>
+                            //step 1 of the 
+                            <div class="container" v-if="showVolet1"></div>
 
                             <hr>
+                                //part2 of the multi step  from question 1 to 15
+                                all the files are not required but if they fill a single input of 
+                                this step they have to fill all of them before going to the next step (biogaz)
+                                <h3>Volet N°1 : POTAGER BIO
+                                </h3>
 
+                                <p>
+                                Ce formulaire a pour but de recenser des foyers pour bénéficier d’un projet de revalorisation de leurs
+                                 déchets domestiques. Ce volet du projet a pour but de permettre aux
+                                  foyers bénéficiaires de revaloriser leurs déchets domestiques en engrais biologique pour
+                                   fertiliser leur potager. <br>
+
+                                   <span>Vous pouvez passer au volet no 2 en cliquant directement sur le bouton
+                                    <strong>Suivant</strong> pour accéder directement au volet Potager si vous 
+                                    n'etes pas interessé par le volet potager.
+                                   </span>
+                                </p>
                             <div class="row g-3">
                                 <div class="col-md-6">
                                     <label for="name" class="text-white">Catégorie socio-professionnelle</label> <span class="red">*</span>
                                     <div class="form-floating">
-                                        <select name="" id="">
+                                        <select name="" id="" class="form-control">
                                             <option value="">Veuillez sélectionner</option>
-                                            <option value="AGriculteur"></option>
+                                            <option value="Agriculteur">Agriculteur</option>
+                                            <option value="Artisan">Artisan</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="email">Votre sexe</label> <span class="red">*</span>
                                     <div class="form-floating">
-                                        <select name="" id="" class="form-control">
+                                        <select name="" id="" class="form-control" v-model="sex">
                                             <option value="">Veuillez sélectionner</option>
                                             <option value="M">Masculin</option>
                                             <option value="F">Féminin</option>
@@ -84,6 +56,11 @@
                                     </div>
                                 </div>
                             </div>
+
+                            //part 2 of the form from question 16 to 19
+                            <h3>
+                               Volet No2 Production  de biogaz
+                            </h3>
 
 
                              <div class="col-12 mt-2">
@@ -92,16 +69,9 @@
                             <input type="text" class="form-control" id="subject" placeholder="Objet">
                         </div>
                     </div>
-                    <div class="col-12 mt-3">
-                    <label for="message">Message</label> <span class="red">*</span>
-                        <div class="form-floating">
-                            <textarea class="form-control" placeholder="Your message" id="message" 
-                            style="height: 150px"></textarea>
-                        </div>
-                    </div>
                     <div class="col-12 mt-3 text-center">
                         <button type="submit" class="btn btn-primary">
-                            Envoyer
+                           Soumettre
                         </button>
                     </div>
                 </form>
@@ -121,7 +91,14 @@ new Vue({
     data: {
         id: <?php echo ($_GET['id']); ?>,
         details: '',
+        FormData: {
+        first_name: '',
+        last_name: '',
+
+        
+        }
     },
+    
     computed: {
 
     },
