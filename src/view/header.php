5bf0afa138760a8652417ff1abd52740278dad34
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-08-14 08:46:29 +0100

    any sin

--- src/view/header.php
+++ src/view/header.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-dark p-2 mb-3">
   <a class="navbar-brand p-2" href="index.php">
-    <img src="public/images/logo_big.jpg" class='logo ' alt="ong 2A2D">
+    <img src="public/images/logo.jpg" class='logo ' alt="ong 2A2D">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <i class="fas fa-bars"></i> 
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto p-1">
+  <div class="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto p-1">
       <li class="nav-item">
         <a class="nav-link text-white" href="index.php?action=home">Accueil</a>
       </li>
@@ -25,6 +25,10 @@
       </li>
     </ul>
   </div>
+
+  <a href="index.php?action=projectPage?id=3" class="btn btn-primary ml-0 right">
+    Projet phare
+  </a>
 </nav>
 
 <style>
